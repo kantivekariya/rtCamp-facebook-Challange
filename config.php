002f7ce36a1e3cd069c08bb6130da45886a6b5ff
kanti <kantivekariya242@gmail.com>
2019-08-08 11:46:43 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
 	require_once "Facebook/autoload.php";
 
 	$FB = new \Facebook\Facebook([
-		'app_id' => '2189180164685340',							//Graph api id
-		'app_secret' => '616edae4d02541f76cf9ec9c18aa1edd',		//Graph api Secret key
+		'app_id' => 'api_id',							//Graph api id
+		'app_secret' => 'app_secreatkey',		//Graph api Secret key
 		'default_graph_version' => 'v3.1'
 	]);
 
